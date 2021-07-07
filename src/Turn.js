f053bcd5fa4c7cd8dde8b4dfba2c624c529ab5e0
Hayley Dixon <hdixon37@gmail.com>
2021-07-06 19:09:00 -0600

    Resolve merge conflicts, merging main with branch

--- src/Turn.js
+++ src/Turn.js
@@ -12,30 +12,20 @@ class Turn{
   };
 
   evaluateGuess(){
-<<<<<<< HEAD
     if(this.guess === this.card.correctAnswer){
       return true
     } else {
         return false
     }
-=======
-    console.log(this.card.correctAnswer)
-    if(this.guess === this.card.correctAnswer){
-      return true
-    } else { return false }
->>>>>>> 13090eb0fb167c93b5225d9b9248ca1500598474
+
   };
 
   giveFeedback(){
     if (this.evaluateGuess()){
       return `correct!`
-<<<<<<< HEAD
     } else {
       return `incorrect!`
     }
-=======
-    } else {return `incorrect!`}
->>>>>>> 13090eb0fb167c93b5225d9b9248ca1500598474
   }
 
 };
