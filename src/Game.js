9ee26e1ad41dede1e5866cfa0d9b7c0cd3223d8b
Hayley Dixon <hdixon37@gmail.com>
2021-07-08 17:28:04 -0600

    Build start method that instiates card, deck and round classes

--- src/Game.js
+++ src/Game.js
@@ -1,9 +1,44 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Round = require('./Round');
+const Deck = require('./Deck');
+
 
 class Game {
-  constructor() {}
+  constructor(questions) {
+    this.questions = questions;
+    this.currentRound = 0;
+  }
+
+  start(){
+    this.currentRound++;
+    let card;
+    let cards = this.questions.map(element => card = new Card(element.id, element.question, element.answers, element.correctAnswer));
+    let deck = new Deck(cards);
+    let round = new Round(deck);
+
+
+
+    // let cards = this.questions.map(element => card = new Card(element.id, element.question, element.answers, element.correctAnswer));
+    // console.log('cards!', cards)
+    //
+    //
+    //
+    // console.log(this.questions[0].question)
+    // let card = new Card(this.questions.id, this.questions.question, this.questions.possibleAnswers, this.questions.correctAnswer );
+
+    // console.log(card)
+    // cards.push(card);
+    // console.log(data.prototypeData)
+    return true
+  }
+
+  instantiateCard(){
+    console.log(cards)
+    return cards;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
--- test/Game-test.js
+++ test/Game-test.js
@@ -6,8 +6,43 @@ const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Round = require('../src/Round');
 const Game = require('../src/Game')
+const data = require('../src/data');
+const prototypeQuestions = data.prototypeData;
+const util = require('../src/util');
+
 describe('Game', function() {
+  let card1;
+  let card2;
+  let card3;
+  let deck;
+  let round;
+  let game;
+
+
+beforeEach(() => {
+
+  game = new Game(prototypeQuestions);
+
+} )
+
+
   it('should be a function', function() {
     expect(Game).to.be.a('function')
   } )
+  it('should be able to keep track of current round', function() {
+    game.start();
+    expect(game.currentRound).to.equal(1);
+  });
+
+  it('should be able to create Cards at start', function() {
+    game.start();
+
+    expect(game.start()).to.equal(true);
+  })
+
+  it('should be able to print message', function() {
+    game.printMessage(deck, round);
+
+  })
+
 })
